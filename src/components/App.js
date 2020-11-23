ef9c30d61eae14e303350f65efa8ebb64efb26bd
nuriadiazcandela <nuriadiazcandela@gmail.com>
2020-11-23 17:18:05 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import '../stylesheet/App.scss';
+
+class App extends React.Component {
+  //   constructor(props) {
+  //     super(props);
+  //   }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hola</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+// import React from 'react';
+// import { Route, Switch } from 'react-router-dom';
+
+// class App extends React.Component {
+//   // ...
+//   render() {
+//     return (
+//       <div>
+//         {/* ... */}
+//         <main>
+//           <Switch>
+//             <Route exact path="/" component={Home} />
+//             <Route path="/about" component={About} />
+//           </Switch>
+//         </main>
+//       </div>
+//     );
+//   }
+// }
